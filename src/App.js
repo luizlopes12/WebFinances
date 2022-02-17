fefdffb738b4cb6a9660a19c98a266f10334da88
Luiz Lopes <luizlopes12@outlook.com>
2022-02-17 16:11:45 -0300

    Estrutura

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import {Container} from './AppStyles'
+import {Header} from './AppStyles'
+import {HeaderText} from './AppStyles'
+import {Body} from './AppStyles'
+
 const App = () =>{
   return(
-    <>
-        
-    </>
+    <Container>
+        <Header>
+          <HeaderText>Sistema Financeiro</HeaderText>
+        </Header>
+        <Body>
+
+        </Body>
+    </Container>
   )
 }
 export default App;
