5286e5070bf7694700e491f8961288084a6d227a
Luiz Lopes <luizlopes12@outlook.com>
2022-02-17 17:04:55 -0300

    table de info

---
+++ src/components/TableArea/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {} from './styles'
+const TableArea = () => {
+  return (
+    <Table>
+        <thead>
+                <tr></tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </Table>
+  )
+}
+
+export default TableArea
