07e69d9182bb27ada877d1d96519d242bb253f56
Luiz Lopes <luizlopes12@outlook.com>
2022-02-17 16:35:07 -0300

    Dados iniciais

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import GlobalStyle from './Global/GlobalStyle'
 import {Container} from './AppStyles'
 import {Header} from './AppStyles'
 import {HeaderText} from './AppStyles'
 import {Body} from './AppStyles'
-
 const App = () =>{
   return(
+    <>
+    <GlobalStyle/>
     <Container>
         <Header>
           <HeaderText>Sistema Financeiro</HeaderText>
         </Header>
         <Body>
+          
+            {/* Area de info */}
+
+            {/* Area de insert */}
+
+            {/* Lista de itens */}
 
         </Body>
     </Container>
+    </>
+
   )
 }
 export default App;
---
+++ src/data/items.js
@@ -0,0 +1,26 @@
+export const items = [
+    {   
+        date: newDate(2022, 3, 17), 
+        category: 'food', 
+        title: 'McDonalds', 
+        value: 32.15
+    },
+    {   
+        date: newDate(2022, 3, 17), 
+        category: 'food', 
+        title: 'Burguer King', 
+        value: 50.12
+    },
+    {   
+        date: newDate(2022, 4, 15), 
+        category: 'rent', 
+        title: 'Aluguel', 
+        value: 1000
+    },
+    {   
+        date: newDate(2022, 4, 18), 
+        category: 'salary', 
+        title: 'Salário fixo', 
+        value: 1800
+    }
+];
